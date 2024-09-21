da2b779a345faf729578236eb48a421e97b3544d
prafulgulani555 <59774145+prafulgulani555@users.noreply.github.com>
2024-09-21 21:49:20 +0530

    zip extractor

---
+++ zip_extractor.py
@@ -0,0 +1,9 @@
+import zipfile
+
+def extract_archive(archive_path, dest_dir):
+    with zipfile.ZipFile(archive_path, 'r') as archive:
+        archive.extractall(dest_dir)
+
+
+if __name__ == "__main__":
+    print("This module helps with extracting zip files")
